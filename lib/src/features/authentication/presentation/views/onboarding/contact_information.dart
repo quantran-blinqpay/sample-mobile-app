87e5c2bb5b822b0c769ced0ca951b4dd5dd6667c
Quan Tran <quan.tranhong@xenia.tech>
2025-09-07 22:56:01 +0700

    push code

--- lib/src/features/authentication/presentation/views/onboarding/contact_information.dart
+++ lib/src/features/authentication/presentation/views/onboarding/contact_information.dart
@@ -1,8 +1,12 @@
 import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:designerwardrobe/src/configs/app_themes/app_images.dart';
+import 'package:designerwardrobe/src/features/authentication/presentation/views/onboarding/select_city_bottom_sheet.dart';
+import 'package:designerwardrobe/src/features/authentication/presentation/views/onboarding/select_state_bottom_sheet.dart';
 import 'package:designerwardrobe/src/router/route_names.dart';
 import 'package:designerwardrobe/src/router/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 @RoutePage(name: contactInformationRoute)
 class ContactInformationScreen extends StatefulWidget {
@@ -16,6 +20,8 @@ class ContactInformationScreen extends StatefulWidget {
 class _ContactInformationScreenState extends State<ContactInformationScreen> {
   final _postalCodeController = TextEditingController();
   final _addressController = TextEditingController();
+  final _stateController = TextEditingController();
+  final _cityController = TextEditingController();
 
   String? _selectedState;
   String? _selectedCity;
@@ -39,17 +45,34 @@ class _ContactInformationScreenState extends State<ContactInformationScreen> {
           icon: const Icon(Icons.arrow_back_ios, color: Colors.black, size: 18),
           onPressed: () => Navigator.pop(context),
         ),
-        actions: const [
+        actions: [
           Padding(
             padding: EdgeInsets.only(right: 16.0),
             child: Center(
-              child: Text(
-                "Step 4 of 4",
-                style: TextStyle(
-                  fontFamily: "Creato Display",
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.black,
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                  children: [
+                    TextSpan(
+                      text:
+                      'Step 3 ',
+                      style: TextStyle(
+                        fontFamily: "Creato Display",
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'of 4',
+                      style: TextStyle(
+                        fontFamily: "Creato Display",
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xff92939E),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -84,31 +107,65 @@ class _ContactInformationScreenState extends State<ContactInformationScreen> {
             const SizedBox(height: 24),
 
             // State dropdown
-            DropdownButtonFormField<String>(
-              decoration: const InputDecoration(labelText: "State"),
-              value: _selectedState,
-              items: states
-                  .map((s) => DropdownMenuItem(value: s, child: Text(s)))
-                  .toList(),
-              onChanged: (value) => setState(() => _selectedState = value),
+            TextFormField(
+              controller: _stateController,
+              readOnly: true,
+              cursorColor: Color(0xff0092FF),
+              onTap: (){
+                _openStateSelector(context);
+              },
+              decoration: InputDecoration(
+                labelText: "State",
+                suffixIcon: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SvgPicture.asset(icQwidArrowDown, width: 24, height: 24, color: Color(0xFF92939E)),
+                ),
+                enabledBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xffF3F5F7), width: 0.5), // custom color & thickness
+                ),
+                focusedBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xff0092FF), width: 1),
+                ),
+              ),
             ),
             const SizedBox(height: 16),
 
             // City dropdown
-            DropdownButtonFormField<String>(
-              decoration: const InputDecoration(labelText: "City"),
-              value: _selectedCity,
-              items: cities
-                  .map((c) => DropdownMenuItem(value: c, child: Text(c)))
-                  .toList(),
-              onChanged: (value) => setState(() => _selectedCity = value),
+            TextFormField(
+              controller: _cityController,
+              readOnly: true,
+              cursorColor: Color(0xff0092FF),
+              onTap: (){
+                _openCitySelector(context);
+              },
+              decoration: InputDecoration(
+                labelText: "City",
+                suffixIcon: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SvgPicture.asset(icQwidArrowDown, width: 24, height: 24, color: Color(0xFF92939E)),
+                ),
+                enabledBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xffF3F5F7), width: 0.5), // custom color & thickness
+                ),
+                focusedBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xff0092FF), width: 1),
+                ),
+              ),
             ),
             const SizedBox(height: 16),
 
             // Postal Code
             TextField(
               controller: _postalCodeController,
-              decoration: const InputDecoration(labelText: "Postal Code"),
+              decoration: const InputDecoration(
+                labelText: "Postal Code",
+                enabledBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xffF3F5F7), width: 0.5), // custom color & thickness
+                ),
+                focusedBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xff0092FF), width: 1),
+                ),
+              ),
               keyboardType: TextInputType.number,
               onChanged: (_) => setState(() {}),
             ),
@@ -117,38 +174,50 @@ class _ContactInformationScreenState extends State<ContactInformationScreen> {
             // Address
             TextField(
               controller: _addressController,
-              decoration: const InputDecoration(labelText: "Address"),
+              decoration: const InputDecoration(
+                labelText: "Address",
+                enabledBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xffF3F5F7), width: 0.5), // custom color & thickness
+                ),
+                focusedBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xff0092FF), width: 1),
+                ),
+              ),
               onChanged: (_) => setState(() {}),
             ),
             const Spacer(),
 
             // Continue button
-            SizedBox(
-              width: double.infinity,
-              height: 52,
-              child: ElevatedButton(
-                onPressed: (){
-                  context.router.push(SecurityAndPinScreenRoute());
-                }/*isFormValid
-                    ? () {
-                  // TODO: Submit contact info
-                }
-                    : null*/,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor:
-                  isFormValid ? const Color(0xFF0092FF) : const Color(0xFFF4F4F4),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
+            SafeArea(
+              child: SizedBox(
+                width: double.infinity,
+                height: 52,
+                child: ElevatedButton(
+                  onPressed: (){
+                    context.router.push(SecurityAndPinScreenRoute());
+                  }/*isFormValid
+                      ? () {
+                    // TODO: Submit contact info
+                  }
+                      : null*/,
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0, // remove shadow
+                    shadowColor: Colors.transparent, // optional, ensures no shadow color
+                    backgroundColor:
+                      isFormValid ? const Color(0xFF0092FF) : const Color(0xFFF4F4F4),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
                   ),
-                ),
-                child: Text(
-                  "Continue",
-                  style: TextStyle(
-                    fontFamily: "Creato Display",
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                    color:
-                    isFormValid ? Colors.white : const Color(0xFFA3A3A3),
+                  child: Text(
+                    "Continue",
+                    style: TextStyle(
+                      fontFamily: "Creato Display",
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      color:
+                      isFormValid ? Colors.white : const Color(0xFFA3A3A3),
+                    ),
                   ),
                 ),
               ),
@@ -158,5 +227,31 @@ class _ContactInformationScreenState extends State<ContactInformationScreen> {
         ),
       ),
     );
+  }
+
+  void _openStateSelector(BuildContext context) async {
+    final selected = await showModalBottomSheet<String>(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (_) => const SelectStateBottomSheet(),
+    );
+
+    if (selected != null) {
+      _stateController.text = selected;
+    }
+  }
+
+  void _openCitySelector(BuildContext context) async {
+    final selected = await showModalBottomSheet<String>(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (_) => const SelectCityBottomSheet(),
+    );
+
+    if (selected != null) {
+      _cityController.text = selected;
+    }
   }
 }
